21da884a9944ca4eb41506db2f8c9fb9765abca8
Nguyen Manh Tan Huynh <nghuynh5079@gmail.com>
2026-01-06 23:47:19 +0700

    chore: initial project structure with 3-layer architecture

---
+++ AEMS_Solution/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AEMS_Solution.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
