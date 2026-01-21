b8f765a4c3df7b555c8e21b4354292702cb4afc7
Nguyen Manh Tan Huynh <nghuynh5079@gmail.com>
2026-01-21 18:00:42 +0700

    #4 feat: implement authentication DTOs

--- BusinessLogic/DTOs/Authentication/Register/RegisterStaffRequest.cs
+++ BusinessLogic/DTOs/Authentication/Register/RegisterStaffRequest.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace BusinessLogic.DTOs.Authentication.Register
 {
-    public class RegisterStudentRequest
+    public class RegisterStaffRequest
     {
         [Required, EmailAddress]
         public string Email { get; set; } = default!;
@@ -19,12 +19,10 @@ namespace BusinessLogic.DTOs.Authentication.Register
         public string FullName { get; set; } = default!;
 
         [Required]
-        public string StudentCode { get; set; } = default!;
+        public string StaffCode { get; set; } = default!;
 
         public string? Phone { get; set; }
 
-        public string? Gender { get; set; }
-
-        public string? Major { get; set; }
+        public string? Position { get; set; }
     }
 }
