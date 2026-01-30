1d310820ca7d2be54a0ac24d9f913b8069d667e3
Khôi Phạm <kpham6488@gmail.com>
2026-01-30 01:52:44 +0000

    Resolve "feat: chatbot(policy)"

--- DataAccess/Migrations/20260127030830_AddAiChatEntities.Designer.cs
+++ DataAccess/Migrations/20260127030830_AddAiChatEntities.Designer.cs
@@ -4,6 +4,7 @@ using DataAccess.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(AEMSContext))]
-    partial class AEMSContextModelSnapshot : ModelSnapshot
+    [Migration("20260127030830_AddAiChatEntities")]
+    partial class AddAiChatEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
