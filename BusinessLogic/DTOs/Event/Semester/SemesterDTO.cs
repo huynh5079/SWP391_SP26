fda829c135f30aa9fcb08b075809e65e154308f4
Khoi Pham <kpham6488@gmail.com>
2026-03-17 17:41:05 +0700

    71:add semesterDTO

---
+++ BusinessLogic/DTOs/Event/Semester/SemesterDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.DTOs.Role.Organizer;
+using DataAccess.Enum;
+
+namespace BusinessLogic.DTOs.Event.Semester
+{
+	public class SemesterDTO
+	{
+		public string? Name { get; set; }
+
+		public string? Code { get; set; }
+
+		public DateTime? StartDate { get; set; }
+
+		public DateTime? EndDate { get; set; }
+
+		public SemesterStatusEnum Status { get; set; }
+
+		public List<EventListDto> EventList { get; set; } = new();
+	}
+}
